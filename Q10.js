f004fca614e73b475b76aaebc5adc9b7403c3ae0
sadafrao <159582784+sadafrao@users.noreply.github.com>
2024-02-14 21:30:04 -0800

    Add files via upload

---
+++ Q10.js
@@ -0,0 +1,15 @@
+//10.Adding Comments : choose two of the programmes you 've Written, add add at least one comment to each . If you don't have anything specific to write 
+// because Ypur programs are two simple at this point , just add your name 
+//and the current data at the top of each program file . then write one sentence 
+// decribing what the programe does.
+// Q10.
+var a = 10;
+var b = 20;
+// addition
+console.log("".concat(a, "+").concat(b, "=").concat(a + b));
+// subtraction
+console.log("".concat(a, "_").concat(b, "=").concat(a - b));
+// multiplication
+console.log("".concat(a, "*").concat(b, "=").concat(a * b));
+//division
+console.log("".concat(a, "/").concat(b, "=").concat(a / b));
---
+++ Q10.ts
@@ -0,0 +1,19 @@
+//10.Adding Comments : choose two of the programmes you 've Written, add add at least one comment to each . If you don't have anything specific to write 
+// because Ypur programs are two simple at this point , just add your name 
+//and the current data at the top of each program file . then write one sentence 
+// decribing what the programe does.
+
+// Q10.
+let a: number = 10
+let b: number = 20
+// addition
+
+console.log(`${a}+${b}=${a + b}`);
+// subtraction
+console.log(`${a}_${b}=${a - b}`);
+// multiplication
+console.log(`${a}*${b}=${a * b}`);
+//division
+console.log(`${a}/${b}=${a / b}`);
+
+
